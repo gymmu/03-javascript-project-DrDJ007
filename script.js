101d41836f08e1258295dd75e3c54d453e0d08aa
Gil Buholzer <e282243@edu.sbl.ch>
2023-11-21 10:21:27 +0100

    Aufgabe 1,2,3 & 4 (kopiert, schon gemacht aber auf falscher Datei)

--- script.js
+++ script.js
@@ -4,7 +4,92 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+
+    if (currentElement === "e") {
+    }
+
+    if (currentElement.toLowerCase() !== "e"){
+      result.push(currentElement)
+    }
+
+    
+   
   }
+  return result.join("")
+  }
+
+
+
+export function aufgabe02 (args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i] 
+  result.push (currentElement.toUpperCase())
+  }
+  return result.join("")
+}
+
+
+export function aufgabe03 (args) {
+  const input = args
+  const result = []
+  let count = 0
+  
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e") { 
+     count = count + 1   
+    }
+
+    else if (currentElement === "E") {
+      count = count + 1
+    }
+
+    }
+  return count
+
+  }
+
+  export function aufgabe04 (args) {
+    const input = args
+    const result = []
+    let count = 1 
+
+
+    for (let i = 0; i < input.length; i++) {
+      const currentElement = input[i]
+
+      if (currentElement === " ") {
+      count = count + 1}
+
+    }
+
+    return 
+  }
+
+export function aufgabe05 (args) {
+  const input = args
+  const result = []
+  
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]}
+
+    if (currentElement === "capitalLetter") {
+    }
+
+    if (currentElement.capitalLetter() !== "capitalLetter"){
+      write: "true"
+    }
+
+    if (currentElement.lowerCase() !== "lowerCase") {
+      write: "false"
+    }
+
+
+
+
+
   return result.join("")
 }
